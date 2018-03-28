0d15265b8f6eb0b84b4364a241de149683b47ca7
Judson Stevens <Judson.Stevens2gmail.com>
2018-03-28 09:16:46 -0600

    Added add interest method and tests

--- lib/patron.rb
+++ lib/patron.rb
@@ -1,10 +1,15 @@
 class Patron
   attr_accessor :interests
+  attr_reader   :name
 
-  def initialize
+  def initialize(name)
     @interests = []
+    @name = name
   end
-  
+
+  def add_interest(interest)
+    @interests << interest
+  end
 
 
 
--- test/patron_test.rb
+++ test/patron_test.rb
@@ -5,7 +5,7 @@ require_relative "../lib/patron.rb"
 
 class PatronTest < MiniTest::Test
   def setup
-    @p = Patron.new
+    @p = Patron.new("Bob")
   end
 
   def test_it_exists
@@ -18,6 +18,26 @@ class PatronTest < MiniTest::Test
     assert_equal expected, actual
   end
 
+  def test_it_has_a_name
+    expected = "Bob"
+    actual = @p.name
+    assert_equal expected, actual
+  end
+
+  def test_it_can_add_interests
+    @p.add_interest("Dead Sea Scrolls")
+    expected = ["Dead Sea Scrolls"]
+    actual = @p.interests
+    assert_equal expected, actual
+  end
+
+  def test_it_can_add_multiple_interests
+    @p.add_interest("Gems and Minerals")
+    @p.add_interest("Dead Sea Scrolls")
+    expected = ["Gems and Minerals", "Dead Sea Scrolls"]
+    actual = @p.interests
+    assert_equal expected, actual
+  end
 
 
 
